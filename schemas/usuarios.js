b8c80bb21f8b1ae4f9013bb36a17b80ffbb00336
Franco Sanchez <francosanchez8102@gmail.com>
2025-03-23 17:55:17 -0300

    feat: create usuarios schema

---
+++ schemas/usuarios.js
@@ -0,0 +1,14 @@
+import z from 'zod';
+
+// rol is not editable here, only in DB
+const usuariosSchema = z.object({
+  dni: z.string().length(8),
+  nombre: z.string().max(60),
+  mail: z.string().email().max(60),
+  telefono: z.string().max(30),
+  contraseña: z.string().max(60),
+});
+
+export function validateUsuarios(data) {
+  return usuariosSchema.safeParse(data);
+}
