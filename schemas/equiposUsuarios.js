0f96c3b92fe514430f31f3276114e14e54d91b70
Franco Sanchez <francosanchez8102@gmail.com>
2025-03-23 18:46:57 -0300

    fix: fix bad attr in equiposUsuarios

--- schemas/equiposUsuarios.js
+++ schemas/equiposUsuarios.js
@@ -1,17 +1,11 @@
 import z from 'zod';
 
+// el atributo capitan se establece en false por defecto
+// solo se puede tener un capitan por equipo
+// el capitan es el usuario que crea el equipo, por lo tanto se establece en la creacion del equipo
 const equiposUsuariosSchema = z.object({
   idEquipo: z.number().int().positive(),
   idUsuario: z.number().int().positive(),
-  capitan: z
-    .union([
-      z.number().int().min(0).max(1),
-      z
-        .string()
-        .transform((val) => Number(val) || 0)
-        .pipe(z.number().int().min(0).max(1)),
-    ])
-    .default(0),
 });
 
 export function validateEquiposUsuarios(data) {
