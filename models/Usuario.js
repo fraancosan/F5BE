78a73a1674fe9f9650c2859a842b550ca43f5fc6
Joaquin Zubiri-PC <joaquinzubiri00@gmail.com>
2025-04-08 10:24:41 -0300

    feat: Create usuario model

---
+++ models/Usuario.js
@@ -0,0 +1,42 @@
+import db from '../database/connection.js';
+import { DataTypes } from 'sequelize';
+
+export const usuarioModel = db.define(
+  'usuario',
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    dni: {
+      type: DataTypes.STRING(8),
+      allowNull: false,
+    },
+    nombre: {
+      type: DataTypes.STRING(60),
+      allowNull: false,
+    },
+    mail: {
+      type: DataTypes.STRING(60),
+      allowNull: false,
+    },
+    telefono: {
+      type: DataTypes.STRING(30),
+      allowNull: false,
+    },
+    contraseña: {
+      type: DataTypes.STRING(60),
+      allowNull: false,
+    },
+    rol: {
+      type: DataTypes.STRING(30),
+      allowNull: false,
+    },
+  },
+  {
+    freezeTableName: false,
+    timestamps: false,
+  },
+);
