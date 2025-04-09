f11186122db9adf1dd9fd0ffef7eb9ec6f673ea2
Joaquin Zubiri-PC <joaquinzubiri00@gmail.com>
2025-04-09 11:24:27 -0300

    feat: create torneo model

---
+++ models/torneo.js
@@ -0,0 +1,35 @@
+import db from '../database/connection.js';
+import { DataTypes } from 'sequelize';
+
+export const torneoModel = db.define(
+  'torneo',
+  {
+    descripcion: {
+      type: DataTypes.STRING(255),
+      allowNull: false,
+    },
+    fechaInicio: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    fechaFin: {
+      type: DataTypes.DATE,
+      allowNull: true,
+      defaultValue: null,
+    },
+    precioInscripcion: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      defaultValue: null,
+    },
+    cantidadEquipos: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      defaultValue: null,
+    },
+  },
+  {
+    freezeTableName: false,
+    timestamps: false,
+  },
+);
