253b86d9160ebe27aea8ff42b36b602123a13904
Joaquin Zubiri-PC <joaquinzubiri00@gmail.com>
2025-04-14 00:10:33 -0300

    feat: create equipoUsuario model

---
+++ models/equipoUsuario.js
@@ -0,0 +1,43 @@
+import db from '../database/connection.js';
+import { DataTypes } from 'sequelize';
+import equipoModel from './equipo.js';
+import usuarioModel from './Usuario.js';
+
+const equipoUsuarioModel = db.define(
+  'EquiposUsuarios',
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    idEquipo: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: equipoModel,
+        key: 'id',
+      },
+    },
+    idUsuario: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: usuarioModel,
+        key: 'id',
+      },
+    },
+    capitan: {
+      type: DataTypes.INTEGER(1),
+      defaultValue: 0,
+      allowNull: false,
+    },
+  },
+  {
+    freezeTableName: false,
+    timestamps: false,
+  },
+);
+
+export default equipoUsuarioModel;
