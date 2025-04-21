e8c05266cb6204df6a983a334e4b2143f3575094
Joaquin Zubiri-PC <joaquinzubiri00@gmail.com>
2025-04-21 17:52:35 -0300

    feat: add cancha router imports to main.js

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import { equipoRouter } from './routes/equipo.js';
 import { torneoRouter } from './routes/torneo.js';
 import { politicaRouter } from './routes/politica.js';
 import { muroRouter } from './routes/muro.js';
+import { canchaRouter } from './routes/cancha.js';
 
 const app = express();
 
@@ -17,6 +18,7 @@ app.use('/equipos', equipoRouter);
 app.use('/torneos', torneoRouter);
 app.use('/politicas', politicaRouter);
 app.use('/muros', muroRouter);
+app.use('/canchas', canchaRouter);
 app.get('/', (_, res) => {
   res.status(404).json({ error: 'Recurso no encontrado' });
 });
