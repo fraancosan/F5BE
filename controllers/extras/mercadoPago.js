807ebfc0018270baa878878c31f4b00b62a34d95
Franco Sanchez <francosanchez8102@gmail.com>
2025-08-08 19:31:51 -0300

    feat: add createPreference function

--- controllers/extras/mercadoPago.js
+++ controllers/extras/mercadoPago.js
@@ -13,6 +13,54 @@ const client = new MercadoPagoConfig({
 
 export class mercadoPagoController {
   /**
+   * Crea las preferencias de pago en MercadoPago a partir de un pedido
+   * @param {Object} param0 Objeto con los datos necesarios para crear la preferencia
+   * @param {string} param0.title Título del pedido
+   * @param {number} param0.precio Precio del pedido
+   * @param {string} param0.idReferencia ID de referencia del pedido
+   * @returns {Object} Preferencia de pago de MercadoPago
+   * @throws {Error} Si ocurre un error al contactar con MercadoPago
+   */
+  static async createPreference({ title, precio, idReferencia }) {
+    try {
+      const now = new Date();
+      const twentyMinutesLater = new Date(now.getTime() + 20 * 60 * 1000);
+      const items = [
+        {
+          id: idReferencia,
+          title: title,
+          quantity: 1,
+          unit_price: precio,
+          currency_id: 'ARS',
+        },
+      ];
+
+      const body = {
+        items,
+        back_urls: {
+          success: process.env.URL_FRONTEND_MP_SUCCESS + '/' + idReferencia,
+          failure: process.env.URL_FRONTEND_MP_FAILURE,
+          pending: process.env.URL_FRONTEND_MP_FAILURE,
+        },
+        notification_url: process.env.URL_BACKEND_MP,
+        auto_return: 'approved',
+        purpose: 'wallet_purchase',
+        statement_descriptor: 'RODO',
+        binary_mode: true,
+        expires: true,
+        expiration_date_from: now.toISOString(),
+        expiration_date_to: twentyMinutesLater.toISOString(),
+        date_of_expiration: twentyMinutesLater.toISOString(),
+        external_reference: idReferencia,
+      };
+      const preference = new Preference(client);
+      return await preference.create({ body });
+    } catch (error) {
+      throw new Error('Ha ocurrido un error al contactar con MercadoPago');
+    }
+  }
+
+  /**
    * It makes a total refund of a payment
    * @param {string} paymentId The payment id to refund
    * @returns {number} The status of the refund
