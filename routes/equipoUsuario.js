0fffbd91a28dc0d3f4ec5d9c38ae025625bda653
Joaquin Zubiri-Note <joaquinzubiri00@gmail.com>
2025-08-22 15:36:43 -0300

    feat: add authentication middleware to equipoUsuario routes

--- routes/equipoUsuario.js
+++ routes/equipoUsuario.js
@@ -1,14 +1,15 @@
 import { Router } from 'express';
 import { equipoUsuarioController } from '../controllers/equipoUsuario.js';
+import { authUser, authAdmin } from '../middlewares/auth.js';
 
 export const equipoUsuarioRouter = Router();
 
-equipoUsuarioRouter.get('/', equipoUsuarioController.getAll);
+equipoUsuarioRouter.get('/', authUser, equipoUsuarioController.getAll);
 
-equipoUsuarioRouter.get('/:id', equipoUsuarioController.getById);
+equipoUsuarioRouter.get('/:id', authAdmin, equipoUsuarioController.getById);
 
-equipoUsuarioRouter.post('/', equipoUsuarioController.create);
+equipoUsuarioRouter.post('/', authUser, equipoUsuarioController.create);
 
-equipoUsuarioRouter.patch('/:id', equipoUsuarioController.update);
+equipoUsuarioRouter.patch('/:id', authUser, equipoUsuarioController.update);
 
-equipoUsuarioRouter.delete('/:id', equipoUsuarioController.delete);
+equipoUsuarioRouter.delete('/:id', authUser, equipoUsuarioController.delete);
